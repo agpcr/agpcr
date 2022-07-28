159a72cc5adfedff0ab127a066618a747a15b392
Hironao Sekine <hiro@alid.co.jp>
2022-07-28 18:40:20 +0900

    changed MissingTeethToggleButton ignore focus

--- src/widget/MissingTeethToggleButton.py
+++ src/widget/MissingTeethToggleButton.py
@@ -5,6 +5,6 @@ class MissingTeethToggleButton(tkinter.Frame):
     def __init__(self, master=None, fdi_number=None, onClick=None):
         super().__init__(master)
 
-        button = tkinter.Button(self, text=fdi_number, fg='black', width='3')
-        button.bind("<Button-1>", onClick)
+        button = tkinter.Button(self, text=fdi_number, fg='black', width='3', takefocus=0)
+        button.bind("<Button-1>", lambda ev, fdi_number=fdi_number: onClick(ev, fdi_number))
         button.pack()
